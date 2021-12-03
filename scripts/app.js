ba63ac81c26d4d7c2ace3b44d93afba1a952c4f9
Cledb Astudillo <cledb.uxcodes@gmail.com>
2021-12-03 09:42:01 +0000

    form submission and player name input validation configured

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,21 @@
 // This file will be the main entry file which will be  executed first. This file will reach out to different HTML elements and store these elements in different variables which we can then point to from a different js file to add our logical programming.//
 
+let editedPlayer = 0;
+
+const players = [{
+        name: '',
+        symbol: 'X'
+    },
+    {
+        name: '',
+        symbol: 'O'
+    },
+];
+
 const playerConfigOverlayElement = document.getElementById('config-overlay');
 const backdropElement = document.getElementById('backdrop');
+const formElement = document.querySelector('form');
+const errorsOutputElement = document.getElementById('config-errors');
 
 const editPlayerOneBtnElement = document.getElementById('edit-player-1-btn');
 const editPlayerTwoBtnElement = document.getElementById('edit-player-2-btn');
@@ -14,3 +28,6 @@ editPlayerTwoBtnElement.addEventListener('click', openPlayerConfig);
 
 cancelConfigBtnElement.addEventListener('click', closePlayerConfig);
 backdropElement.addEventListener('click', closePlayerConfig);
+
+formElement.addEventListener('submit',
+    savePlayerConfig);
