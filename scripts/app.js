98cb983df7db9611fd5966e33ed4184e8048f9c4
Cledb Astudillo <cledb.uxcodes@gmail.com>
2021-12-04 06:18:48 +0000

    written code to add error message when user clicks start new game button when the player names are not set

--- scripts/app.js
+++ scripts/app.js
@@ -16,11 +16,15 @@ const playerConfigOverlayElement = document.getElementById('config-overlay');
 const backdropElement = document.getElementById('backdrop');
 const formElement = document.querySelector('form');
 const errorsOutputElement = document.getElementById('config-errors');
+const gameAreaElement = document.getElementById('active-game');
+const startNewGameErrorMessage = document.getElementById('start-game-error-message');
 
 const editPlayerOneBtnElement = document.getElementById('edit-player-1-btn');
 const editPlayerTwoBtnElement = document.getElementById('edit-player-2-btn');
 
 const cancelConfigBtnElement = document.getElementById('cancel-config-btn');
+
+const startNewGameBtnElement = document.getElementById('start-game-btn');
 
 
 editPlayerOneBtnElement.addEventListener('click', openPlayerConfig);
@@ -31,3 +35,6 @@ backdropElement.addEventListener('click', closePlayerConfig);
 
 formElement.addEventListener('submit',
     savePlayerConfig);
+
+
+startNewGameBtnElement.addEventListener('click', startNewGame);
--- scripts/game.js
+++ scripts/game.js
@@ -1,1 +1,12 @@
-// This will be responsible for holding all the logic related to configuring the game itself.// 
+// This will be responsible for holding all the logic related to configuring the game itself.//
+
+function startNewGame() {
+    if (players[0].name === '' || players[1].name === '') {
+        startNewGameErrorMessage.textContent = 'Ooops! You haven\'t entered both player names yet!';
+        return;
+    } else {
+        startNewGameErrorMessage.style.display = 'none';
+    }
+    gameAreaElement.style.display = 'block';
+
+}
