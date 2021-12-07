20d6638bf333ab495da93582fa77a272ee047e2f
Cledb Astudillo <cledb.uxcodes@gmail.com>
2021-12-07 06:10:47 +0000

    code added for tracking who's the player selecting game boards' field element. That tracking will then store that data and check if there's a winner or is a draw. Also added code for when there's a winner and for restarting the game. Also added logic for when it's gameover(there's a winner) the game fields are not clickable...Project finished...:)

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,15 @@
 // This file will be the main entry file which will be  executed first. This file will reach out to different HTML elements and store these elements in different variables which we can then point to from a different js file to add our logical programming.//
 
+const gameData = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0],
+];
+
 let editedPlayer = 0;
 let activePlayer = 0;
+let currentRound = 1;
+let gameIsOver = false;
 
 const players = [{
         name: '',
@@ -20,6 +28,7 @@ const errorsOutputElement = document.getElementById('config-errors');
 const gameAreaElement = document.getElementById('active-game');
 const startNewGameErrorMessage = document.getElementById('start-game-error-message');
 const activePlayerNameElement = document.getElementById('active-player-name');
+const gameOverElement = document.getElementById('game-over');
 
 const editPlayerOneBtnElement = document.getElementById('edit-player-1-btn');
 const editPlayerTwoBtnElement = document.getElementById('edit-player-2-btn');
@@ -28,6 +37,7 @@ const cancelConfigBtnElement = document.getElementById('cancel-config-btn');
 
 const startNewGameBtnElement = document.getElementById('start-game-btn');
 const gameFieldElements = document.querySelectorAll('#game-board li');
+//const gameBoardElement = document.getElementById('game-board');
 
 
 editPlayerOneBtnElement.addEventListener('click', openPlayerConfig);
